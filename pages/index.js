6ca813f23292f02a93a4cca1c0d9da245d2130d1
shilei <240638896@qq.com>
2020-06-02 16:36:55 +0800

    next-r

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import { Button } from 'antd'
+
+export default function Home() {
+  return (
+      <div>
+        <Button type={'primary'}>按钮er</Button>
+      </div>
+  )
+}
